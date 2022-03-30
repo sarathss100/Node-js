a29d5b53f5d68a6a38f45bd519a5b057488f3727
SarathSS <sarathss100@gmail.com>
2022-03-30 07:00:59 +0530

    30032022

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-console.log("Hellp");
+// let number = 10;
+// number += 2;
+// console.log("Result : " + number);
+
+// let nameString = new String("Hello");
+// it's actually an object like in class object in java
+
+// console.log(nameString.length);
+// length it's a function inside the string class
+
+// let num1 = 10;
+// let num2 = 20;
+// const sum = num1 + num2;
+// console.log("Sum of two numbers is : " + sum);
+
+// let number = 0;
+// if (number > 0) {
+//     console.log("It is a positive number");
+// } else if (number === 0) {
+//     console.log("It is a neutral number");
+// } else {
+//     console.log("It is a negative number");
+// }
+
+// let read = require('readline-sync');
+// let input = read.question("Please enter your name : ");
+// console.log("Your name is " + input);
+
+// let read = require('readline-sync');
+// let number = read.question("Please enter a number : ");
+// if (number < 0) {
+//     console.log("Please enter a negative number");
+// } else if (number > 0) {
+//     console.log("Please enter a positive number");
+// } else {
+//     console.log("Please enter a neutral number");
+// }
+
+// function message() {
+//     console.log("Hello");
+// }
+// message();
+
+// let car = {type:"Fiat", model:"500", color:"green", 
+// property: function(){
+//     console.log("This is a car object");
+// }};
+// console.log(car.type);
+// console.log(car["model"]);
+// console.log(car.property);
+
+// let text = "Hello";
+// let answer = "It's a car";
+// console.log(answer);
+// console.log(answer.length);
+
+// let text1 = "It's so called \'Hello\'";
+// console.log(text1);
+
+let fullName = new String("Hello");
+console.log(fullName);
